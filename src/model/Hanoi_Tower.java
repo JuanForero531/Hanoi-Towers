8092d801433e4dc889b266f7623b7a289566acfc
JuanForero531 <111099370+JuanForero531@users.noreply.github.com>
2023-08-26 09:22:15 -0500

    first commit

---
+++ src/model/Hanoi_Tower.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author forer
+ */
+public class Hanoi_Tower {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
