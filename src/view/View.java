66daa63fcd477645765f16aa4cd47fd26e39dc97
JuanForero531 <111099370+JuanForero531@users.noreply.github.com>
2023-08-30 19:10:23 -0500

    Update View.java

--- src/view/View.java
+++ src/view/View.java
@@ -6,88 +6,74 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
-public class View extends JFrame {
-    private JLabel titleLabel;
-    private JPanel levelPanel;
-    private JLabel levelLabel;
-    private JComboBox<String> levelComboBox;
-    public JButton startButton;
-    private JTable solutionTable;
-    private JScrollPane tableScrollPane;
-    private JLabel imageLabel;
-    private JLabel developersLabel; // Etiqueta para los nombres de los desarrolladores
-    private DefaultTableModel tableModel;
+public class View extends JPanel {
 
-    public View() {
-        setTitle("El Desafío de la Torre de Hanoi");
-        setSize(1200, 600);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new BorderLayout());
+    private int contadorMovimientos = 0;
+    private JLabel contadorLabel = new JLabel("Movimientos: 0");
+    private boolean juegoGanado = false;
 
-        // Cambiar color de fondo a azul claro
-        getContentPane().setBackground(new Color(176, 224, 230));
+ public View() {
+    //nuevo
+        JLabel nombres = new JLabel("Desarrolladores: Juan José Forero, Samuel Vargas, Sebastian Daza");
+        add(nombres, BorderLayout.NORTH);
+        contadorLabel.setFont(new Font("Arial", Font.PLAIN, 12));
+        contadorLabel.setBounds(10, 100, 150, 20);
+        add(contadorLabel);
+     ...
+ }
 
-        developersLabel = new JLabel("Desarrollado por: Juan José Forero, Samuel Vargas y Sebastian Daza"); // Nombres de los desarrolladores
-        developersLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        add(developersLabel, BorderLayout.NORTH);
+       public void mouseReleased(MouseEvent e) {
+           //nuevo
+           if (discoSeleccionado != null) {
+                    int distanciaX = Math.abs(e.getX() - 350);
+                    if (discoSeleccionado.getColor() == Color.BLUE && distanciaX <= 10) {
+                        juegoGanado = true;
+                        repaint();
+                    }
+            }
+      }
 
-        titleLabel = new JLabel("Torre de Hanoi", SwingConstants.LEFT);
-        titleLabel.setFont(new Font("Arial", Font.BOLD, 30));
-        add(titleLabel, BorderLayout.WEST);
+     public void mousePressed(MouseEvent e) {
+             //nuevo
+                contadorMovimientos++;
+                contadorLabel.setText("Movimientos: " + contadorMovimientos);
 
-        JPanel centerPanel = new JPanel();
-        centerPanel.setLayout(new FlowLayout());
+         
+                for (Disco modelo : discosMovibles) {
+                    if (modelo.getX() <= e.getX() && e.getX() <= modelo.getX() + modelo.getWidth() &&
+                            modelo.getY() <= e.getY() && e.getY() <= modelo.getY() + modelo.getHeight()) {
+                        discoSeleccionado = modelo;
+                        break;
+                    }
+                }
+            }
 
-        levelPanel = new JPanel();
-        levelPanel.setBackground(new Color(70, 130, 180));
-        levelPanel.setLayout(new FlowLayout());
-        levelLabel = new JLabel("Nivel:");
-        levelPanel.add(levelLabel);
-        levelComboBox = new JComboBox<>(new String[]{"Principiantes", "Novato", "Intermedio", "Avanzado", "Experto", "Maestro"});
-        levelPanel.add(levelComboBox);
-        centerPanel.add(levelPanel);
+     @Override
+    protected void paintComponent(Graphics g) {
+        //nuevo
+        g.drawString("Movimientos: " + contadorMovimientos, 10, 100);
 
-        startButton = new JButton("Iniciar");
-        centerPanel.add(startButton);
-
-        add(centerPanel, BorderLayout.CENTER);
-
-        tableModel = new DefaultTableModel();
-        tableModel.addColumn("Número de Movimiento");
-        tableModel.addColumn("Número de Disco");
-        tableModel.addColumn("Desde");
-        tableModel.addColumn("Hasta");
-        solutionTable = new JTable(tableModel);
-
-        // Cambiar color de fondo de los títulos de las tablas a azul oscuro
-        DefaultTableCellRenderer headerRenderer = new DefaultTableCellRenderer();
-        headerRenderer.setBackground(new Color(70, 130, 180));
-        for (int i = 0; i < solutionTable.getColumnModel().getColumnCount(); i++) {
-            solutionTable.getColumnModel().getColumn(i).setHeaderRenderer(headerRenderer);
+        super.paintComponent(g);
+        for (Disco discoNoMovible : discosNoMovibles) {
+            g.setColor(discoNoMovible.getColor());
+            g.fillRect(discoNoMovible.getX(), discoNoMovible.getY(), discoNoMovible.getWidth(), discoNoMovible.getHeight());
         }
+        for (Disco discoFijo : discosFijos) {
+            g.setColor(discoFijo.getColor());
+            g.fillRect(discoFijo.getX(), discoFijo.getY(), discoFijo.getWidth(), discoFijo.getHeight());
+        }
+        for (Disco discoMovible : discosMovibles) {
+            g.setColor(discoMovible.getColor());
+            g.fillRect(discoMovible.getX(), discoMovible.getY(), discoMovible.getWidth(), discoMovible.getHeight());
 
-        tableScrollPane = new JScrollPane(solutionTable);
-        add(tableScrollPane, BorderLayout.SOUTH);
-
-        ImageIcon imageIcon = new ImageIcon("src\\view\\hanoi_image.jpeg");
-        Image image = imageIcon.getImage();
-        Image scaledImage = image.getScaledInstance(500, 150, Image.SCALE_SMOOTH);
-        imageIcon.setImage(scaledImage);
-        imageLabel = new JLabel(imageIcon);
-        add(imageLabel, BorderLayout.EAST);
-
-        setVisible(true);
+            //nuevo
+            if (juegoGanado) { 
+                g.setColor(Color.RED);
+                g.setFont(new Font("Arial", Font.BOLD, 20));  
+                g.drawString("¡Has ganado!", 200, 150);
+            }
+        }
     }
 
-    public void addActionListener(ActionListener listener) {
-        startButton.addActionListener(listener);
-    }
-
-    public int getSelectedLevelIndex() {
-        return levelComboBox.getSelectedIndex();
-    }
-
-    public DefaultTableModel getTableModel() {
-        return tableModel;
-    }
+  
 }
