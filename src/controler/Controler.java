f59f39fa55584129190bfd4ac671b68296bd8aa2
SamVargasGit <136205680+SamVargasGit@users.noreply.github.com>
2023-08-30 22:32:47 -0500

    controler_and_view_modify

--- src/controler/Controler.java
+++ src/controler/Controler.java
@@ -1,4 +1,4 @@
-package controller;
+package controler;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
--- src/model/Disco.java
+++ src/model/Disco.java
@@ -12,64 +12,40 @@ public class Disco {
     private int width;
     private int height;
 
-    public Disco(){
-
-    }
     public Disco(int x, int y, Color color, int width, int height) {
         this.x = x;
         this.y = y;
         this.color = color;
         this.width = width;
         this.height = height;
-        this.posXOriginal = x; // Guardar la posición original en X
-        this.posYOriginal = y; // Guardar la posición original en Y
+        this.posXOriginal = x;
+        this.posYOriginal = y;
     }
     public int getX() {
         return x;
     }
-    public int getPosXAnterior() {
-        return posXAnterior;
-    }
-    public int getPosYAnterior() {
-        return posYAnterior;
-    }
     public int getPosXOriginal() {
         return posXOriginal;
     }
-
     public int getPosYOriginal() {
         return posYOriginal;
     }
-    public void setPosXAnterior(int posXAnterior) {
-        this.posXAnterior = posXAnterior;
-    }
-
-    public void setPosYAnterior(int posYAnterior) {
-        this.posYAnterior = posYAnterior;
-    }
-
     public int getY() {
         return y;
     }
-
     public Color getColor() {
         return color;
     }
-
     public int getWidth() {
         return width;
     }
-
     public int getHeight() {
         return height;
     }
-
     public void setPosicion(int x, int y) {
         posXAnterior = this.x;
         posYAnterior = this.y;
         this.x = x;
         this.y = y;
     }
-
 }
-
